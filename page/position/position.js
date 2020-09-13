88a4be718dd7c29a06bf9d03dc5da86e9e3fb756
fenghaifeng <1784542447@qq.com>
2020-09-13 17:47:35 +0800

    update

--- page/position/position.js
+++ page/position/position.js
@@ -1,10 +1,11 @@
+const app = getApp();
 Page({
   data: {
     navList: [1, 2, 3, 4, 5],
     showCengId: 1,//默认选择第一层
     cengList: [1, 2, 3, 4],//容器层数
-    tag: 12,//9*9  12*12
-    items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], //样本盒
+    samplebox: 1,//1 9*9   2 12*12
+    items: [], //样本盒
     cells: Array.apply(null, {
       length: 81
     }).map(function (_, index) {
@@ -13,17 +14,68 @@ Page({
         number: index % 9 + 1
       };
     }),
-   rangeArr: ['房间301','房间302'],//弹窗列表
-    val: 0,//选择第几个
-    rangeArr1: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
-    val1:[0,0],
+    roomList: [],//房间列表
+    val: 1,//选择第几个
+    containerList: [
+      {
+        id: 10,
+        name: '冰箱'
+      },
+      // {
+      //   id:20,
+      //   name:'液氮罐'
+      // },
+      // {
+      //   id:30,
+      //   name:'柜子'
+      // },
+      // {
+      //   id:40,
+      //   name:'冷库'
+      // },
+      // {
+      //   id:50,
+      //   name:'石蜡块档案柜'
+      // },
+      // {
+      //   id:60,
+      //   name:'病理玻片柜'
+      // },
+    ],
+    val1: 0,
+    bxList: [],//容器
+    val2: 0,
+    containerid: null,//容器id
+    levela: null,//容器大层
+    levelb: null,//容器小层
+    option: null,//传值
+    ctList: [],//抽屉
+    showXiaoCengId: null,//小层选中
+    jiazList: [],//架子
+    heziList: [],//盒子
+    jiaziId: null,//架子Id
+    showJiaziId: null,//架子选中
   },
-  onLoad() {
-
-
+  onShow() {
+    this.getRoomList();
+  },
+  onLoad(option) {
+    console.log(option);
+    if (option) {
+      this.setData({
+        option: option
+      })
+    }
     let tempArr3 = [];
-    let tempArr = this.data.items;
-    let tempArr1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J','K','L','M'];
+    let tempArr = [];
+    let tempArr1 = [];
+    if (this.data.samplebox == 1) {
+      tempArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      tempArr1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J'];
+    } else {
+      tempArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+      tempArr1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M'];
+    }
     tempArr1.forEach((item1, index1) => {
       tempArr.forEach((item, index) => {
         tempArr3.push({
@@ -37,18 +89,151 @@ Page({
       items: tempArr3
     })
   },
-  // 获取弹窗
-  change1(e) {
+  // 获取房间号
+  getRoomList() {
+    app.http('/api/container/getlistbyroom', {
+      name: '',
+      roomid: 0,
+      pageIndex: 1,
+      pageSize: 1000,
+      fldSort: '',
+      fldName: ''
+    }).then(res => {
+      if (res.data.success) {
+        this.setData({
+          roomList: res.data.result.pageData,
+        })
+        this.getBxList();
+      }
+    })
+  },
+  // 房间号
+  change(e) {
     this.setData({
       val: e.detail.value
     })
+    this.getBxList();
+  },
+  // 容器类型
+  change1(e) {
+    this.setData({
+      val1: e.detail.value
+    })
+  },
+  // 容器
+  change2(e) {
+    this.setData({
+      val2: e.detail.value
+    })
+    console.log(e)
+  },
+  // 获取冰箱列表
+  getBxList() {
+    app.http('/api/container/getlistbyfridge', {
+      name: '',
+      roomid: this.data.val == 0 ? this.data.roomList[0].id : this.data.val,
+      pageIndex: 1,
+      pageSize: 1000,
+      fldSort: '',
+      fldName: ''
+    })
+      .then((res) => {
+        if (res.data.success) {
+          this.setData({
+            bxList: res.data.result.pageData,
+            containerid: res.data.result.pageData.length > 0 ? res.data.result.pageData[this.data.val2].containerid : null,
+            levela: res.data.result.pageData.length > 0 ? res.data.result.pageData[this.data.val2].levela : null,
+            levelb: res.data.result.pageData.length > 0 ? res.data.result.pageData[this.data.val2].levelb : null,
+            samplebox: res.data.result.pageData.length > 0 ? res.data.result.pageData[this.data.val2].samplebox : null,
+          })
+          if (this.data.levela) {
+            this.changeLeava(this.data.showCengId);
+          }
+        }
+      });
   },
   showItem(e) {
     console.log(e.currentTarget.dataset)
+    this.setData({
+      showHeziId: e.currentTarget.dataset.index
+    })
   },
+  // 大层切换
   changeCeng(e) {
     this.setData({
       showCengId: e.currentTarget.dataset.id
+    })
+    this.changeLeava(this.data.showCengId);
+  },
+  // 从大层获取小层
+  changeLeava(showCengId) {
+    app.http('/api/container/getconbylevela', {
+      containerid: this.data.containerid,
+      level_a: showCengId,
+    }).then(res => {
+      if (res.data.success) {
+        this.setData({
+          ctList: res.data.result
+        })
+      }
+    })
+  },
+  // 从小层获取架子
+  getJiazi(e) {
+    let item = e.currentTarget.dataset.item;
+    app.http('/api/container/getconbylevelb', {
+      containerid: this.data.containerid,
+      level_a: this.data.showCengId,
+      level_b: item.level_b,
+      level_x: item.level_x,
+    }).then(res => {
+      if (res.data.success) {
+        this.setData({
+          jiazList: res.data.result,
+          showXiaoCengId: e.currentTarget.dataset.index
+        })
+      }
+    })
+  },
+  // 获取盒子
+  getHezi(e) {
+    let item = e.currentTarget.dataset.item;
+    app.http('/api/container/getconbylattice', {
+      latticeid: item.id,
+    }).then(res => {
+      if (res.data.success) {
+        this.setData({
+          jiaziId: item.id,
+          heziList: res.data.result,
+          showJiaziId: e.currentTarget.dataset.index
+        })
+      }
+    })
+  },
+  // 入库
+  ruku() {
+    app.http('/api/apply/applyfirst',{
+      apply_reason:'',
+      remarks:'',
+      product_type_id:1,
+      product_type:'样本',
+      instorage:{
+        sample:[
+          {
+            sample_id:parseInt(this.data.option.sample_id),
+            sample_type_id:parseInt(this.data.option.sample_type_id),
+            sample_type:this.data.option.sample_type,
+            sample_no:this.data.option.sample_no,
+            ac_frozenbox:this.data.jiaziId,
+            ac_sample:this.data.showHeziId,
+            qrcode:this.data.option.code
+          }
+        ]
+      }
+    }).then(res=>{
+      if (res.data.success) {
+        
+      }
     })
   },
 });
--- page/search/search.js
+++ page/search/search.js
@@ -200,21 +200,39 @@ Page({
     ],
     date1: '',
     date2: '',
+    pageIndex: 1,
+    totalPageCount: 0,
     toggle1: false,//展开收起
     showDialog: false,//展开筛选
     select1: false,//全选
     pageData: null,//数据
     count: 0,//勾选条数
+    type: null,//1首次入库申请单 2转移申请单 3挂失申请表 4归还申请表 5延期申请表 6实验取出申请表 7过期销毁申请表  8消耗完申请  (3、4、5、8获取列表接口不一样)
+
+    state: null,//样本状态 1库存中  2未入库 3取用中 4已销毁 5已挂失 6逾期中 7已过期
   },
-  onLoad() {
-    console.log(this);
-    this.getData();
+  onLoad(option) {
+    if (option.type) {
+      this.setData({
+        type: option.type
+      })
+    }
+    if (option.state) {
+      this.setData({
+        state: option.state
+      })
+    }
+    if (option.type == 1) {
+      dd.setNavigationBar({ title: '待入库列表' })
+    }
+    this.getData(1);
   },
   // 获取数据
-  getData() {
+  getData(pageIndex) {
     app.http('/api/sample/getsamplelist', {
       name: this.data.keyword,
-      pageIndex: 1,
+      state: parseInt(this.data.state),
+      pageIndex: pageIndex,
       pageSize: 10,
       fldSort: '',
       fldName: ''
@@ -224,8 +242,12 @@ Page({
         tempArr.forEach(item => {
           item.select = false;
         })
+        let temp = this.data.pageData;
+
         this.setData({
-          pageData: tempArr
+          pageData: pageIndex == 1 ? tempArr : temp.concat(tempArr),
+          pageIndex: res.data.result.pageIndex,
+          totalPageCount: res.data.result.totalPageCount,
         })
       }
     })
@@ -234,7 +256,24 @@ Page({
     this.setData({
       keyword: ''
     })
-    this.getData();
+    this.getData(1);
+  },
+  // 扫码
+  saoma(e) {
+    // console.log(e)
+    let item = e.currentTarget.dataset.item;
+    dd.navigateTo({
+      url: '/page/position/position?code=http://weiwue.com/s/dl&type=' + this.data.type + '&sample_id=' + item.id + '&sample_type_id=' + item.typeid + '&sample_type=' + item.type + '&sample_no=' + item.no + '&name=' + item.name + '&tissuestype=' + item.tissuestype
+    })
+    // dd.scan({
+    //   type: 'qr',
+    //   success: (res) => {
+    //     console.log(res.code)
+    //     dd.navigateTo({
+    //       url: '/page/position/position?code=' + res.code + '&type=' + this.data.type + '&sample_id=' + item.id + '&sample_type_id=' + item.typeid + '&sample_type=' + item.type + '&sample_no=' + item.no + '&name=' + item.name + '&tissuestype=' + item.tissuestype
+    //     })
+    //   },
+    // });
   },
   setChange(e) {
     let tempArr = this.data.pageData;
@@ -276,7 +315,7 @@ Page({
   },
   // 搜索
   search() {
-    this.getData();
+    this.getData(1);
   },
   // 获取日期
   change1(e) {
@@ -350,37 +389,44 @@ Page({
     })
   },
   onPullDownRefresh() {
-    // neil.http('post','/api/sample/getsamplelist',{
-    //     name:'',
-    //     pageIndex:1, 
-    //     pageSize:10,
-    //     fldSort:'',
-    //     fldName:''
-    //   }).then(res=>{
-    //     console.log(res)
-    //   })
+    this.getData(1);
     // 页面被下拉
     dd.stopPullDownRefresh();
   },
   onReachBottom() {
+    if (this.data.totalPageCount < this.data.pageIndex) {
+      return
+    }
+    let temp = this.data.pageIndex;
+    temp++;
+    this.setData({
+      pageIndex: temp
+    })
+    this.getData(temp);
     // 页面被拉到底部
   },
   submit() {
     let pages = getCurrentPages();//页面路由
     let tempArr = this.data.pageData;
-    let a = tempArr.filter(item=>{
-      return item.select ==true
-    }).map(item=>{
-      return item
+    let tempArr1 = [];
+    tempArr.filter(item => {
+      return item.select == true
+    }).map(item => {
+      tempArr1.push({
+        sample_id: item.id,
+        sample_type_id: item.typeid,
+        sample_type: item.type,
+        sample_no: item.no,
+      })
     })
-    console.log(a)
     let prevPage = pages[pages.length - 2];
     prevPage.setData({
-      list:a,
-      unfold:true
+      list: tempArr1,
+      unfold: true,
+      type: this.data.type
     })
     dd.navigateBack({
-      delta:1
+      delta: 1
     })
     console.log(prevPage);
   },
